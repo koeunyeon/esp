46c35197dd45357c82c510eb388fef6053ee4c37
koeunyeon <koeunyeon@gmail.com>
2021-04-21 15:38:25 +0900

    회원가입 / 로그인에 헤더/푸터 추가

--- src/user/regist.php
+++ src/user/regist.php
@@ -4,8 +4,10 @@ if ($result){
     ESP::redirect("/user/login");
 }
 ?>
+<?php ESP::part_header(); ?>
 <form method="POST">
     <p>user_id : <input type="text" name="login_id" id="login_id" value="<?= ESP::param("login_id") ?? "" ?>" /></p>
     <p>user_pw : <input type="password" name="login_pw" id="login_pw"  value="<?= ESP::param("login_pw") ?? "" ?>"/></p>
     <p><input type="submit" value="회원가입" /></p>
 </form>
+<?php ESP::part_footer(); ?>
