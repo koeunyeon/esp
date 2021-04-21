c59d4dc5344e04301c0406ffb338762e0f01eb92
koeunyeon <koeunyeon@gmail.com>
2021-04-21 10:54:48 +0900

    edit 예제 의미없는 css class 삭제

--- src/article/edit.php
+++ src/article/edit.php
@@ -4,7 +4,7 @@ $model = ESP::auto_find();
 ?>
 <?php ESP::part_header(); ?>
 <form method="POST">
-    <p>title : <input type="text" name="title" id="title" class="md5" value="<?= $model->title ?>" /></p>
+    <p>title : <input type="text" name="title" id="title" value="<?= $model->title ?>" /></p>
     <p>content : <input type="text" name="content" id="content" value="<?= $model->content ?>" /></p>
     <input type="submit" value="저장" />
 </form>
