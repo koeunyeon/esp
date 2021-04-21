979e3be0be7d8a5ae143a9a6f4caddba1be71562
koeunyeon <koeunyeon@gmail.com>
2021-04-21 10:59:25 +0900

    list 예제 수정

--- src/article/list.php
+++ src/article/list.php
@@ -1,13 +1,14 @@
 <?php ESP::part_header(); ?>
 <?php
-$page = ESP::param_uri(0, "1");
-$all = ESP::db()->all();
-ESP::html_ul_open();
-foreach ($all as $row) {
-    ESP::part_auto("row", $row->items());
-}
-ESP::html_ul_close();
+$page_list = ESP::auto_pagenate();
 ?>
+<ul>
+    <?php
+    foreach ($page_list as $row) {
+        ESP::part_auto("row", $row->items());
+    }
+    ?>
+</ul>
 <p><a href="<?= ESP::link_create() ?>">만들기</a></p>
 
 <?= ESP::part_footer(); ?>
